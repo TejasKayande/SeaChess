5a6d5d5753ea53be33dd765cad49900a69fad842
Tejas Kayande <tskayande@gmail.com>
2026-06-10 15:23:29 +0530

    Add Web Build in docs/

--- build-web/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Tejas/Marcal/Probes/SeaChess
-# Build directory: C:/Users/Tejas/Marcal/Probes/SeaChess/build-web
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("dependencies/raylib")
--- build-web/dependencies/raylib/raylib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Tejas/Marcal/Probes/SeaChess/dependencies/raylib/src
-# Build directory: C:/Users/Tejas/Marcal/Probes/SeaChess/build-web/dependencies/raylib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
